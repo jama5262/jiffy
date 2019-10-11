899d742a915ca312acf9a2132bcb46f540f4a935
Jama Mohamed <jama3137@gmail.com>
2019-10-11 14:06:06 +0300

    Set up travis and coveralls

---
+++ tools/travis.sh
@@ -0,0 +1,18 @@
+# Run pub get to fetch packages.
+pub get
+
+# Verify that the libraries are error and warning-free.
+echo "Running dartanalyzer..."
+dartanalyzer lib test example
+
+# Verify that dartfmt has been run.
+echo "Checking dartfmt..."
+if [[ $(dartfmt -n --set-exit-if-changed lib test example) ]]; then
+	echo "Failed dartfmt check"
+	exit 1
+
+fi
+
+# Run the tests.
+echo "Running tests..."
+pub run test_coverage --no-badge
