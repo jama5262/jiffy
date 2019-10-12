86817277d1ce3f966099b09d8afcc71b7c5b6ce5
Jama Mohamed <jama3137@gmail.com>
2019-10-12 18:31:57 +0300

    Setup the get datetime tests

--- test/jiffy_test.dart
+++ test/jiffy_test.dart
@@ -1,4 +1,31 @@
 import 'package:jiffy/jiffy.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group('Test get datetime methods', () {
+    test("Get seconds", () {
+      expect(Jiffy().seconds, DateTime.now().second);
+    });
+    test("Get seconds", () {
+      expect(Jiffy().seconds, DateTime.now().second);
+    });
+    test("Get minutes", () {
+      expect(Jiffy().minute, DateTime.now().minute);
+    });
+    test("Get hours", () {
+      expect(Jiffy().hour, DateTime.now().hour);
+    });
+    test("Get date", () {
+      expect(Jiffy().date, DateTime.now().day);
+    });
+    test("Get day", () {
+      expect(Jiffy().day, DateTime.now().weekday);
+    });
+    test("Get month", () {
+      expect(Jiffy().month, DateTime.now().month);
+    });
+    test("Get year", () {
+      expect(Jiffy().year, DateTime.now().year);
+    });
+  });
+}
