1aa3154459d75afe87203267eb447426b7b7c989
Jama Mohamed <jama3137@gmail.com>
2020-01-08 11:03:03 +0300

    [Finished] Added tests to pl_locale

--- lib/src/relative_time/locales/pl_locale.dart
+++ lib/src/relative_time/locales/pl_locale.dart
@@ -9,44 +9,48 @@ class PlLocale extends LookUpMessages {
   String aboutAMinute(int minutes) => 'minutę';
   String minutes(int minutes) {
     String result;
-    if(minutes >= 1 && minutes <= 4) {
+    if (minutes >= 1 && minutes <= 4) {
       result = "minuty";
     } else {
       result = "minut";
     }
     return "$minutes $result";
   }
+
   String aboutAnHour(int minutes) => 'godzinę';
   String hours(int hours) {
     String result;
-    if(hours >= 1 && hours <= 4) {
+    if (hours >= 1 && hours <= 4) {
       result = "godziny";
     } else {
       result = "godzin";
     }
     return "$hours $result";
   }
+
   String aDay(int hours) => '1 dzień';
   String days(int days) => '$days dni';
   String aboutAMonth(int days) => 'miesiąc';
   String months(int months) {
     String result;
-    if(months >= 1 && months <= 4) {
+    if (months >= 1 && months <= 4) {
       result = "miesiące";
     } else {
       result = "miesięcy";
     }
     return "$months $result";
   }
+
   String aboutAYear(int year) => 'rok';
   String years(int years) {
     String result;
-    if(years >= 1 && years <= 4) {
+    if (years >= 1 && years <= 4) {
       result = "lata";
     } else {
       result = "lat";
     }
     return "$years $result";
   }
+
   String wordSeparator() => ' ';
 }
