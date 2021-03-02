ed148375f5bd6c049cfd9b737382d7d863836db7
Jama Mohamed <jama3137@gmail.com>
2021-03-02 16:11:50 +0300

    [Finished] Format files

--- example/jiffy_example.dart
+++ example/jiffy_example.dart
@@ -3,10 +3,11 @@ import 'package:jiffy/src/enums/units.dart';
 
 Future<int> main() async {
 //  DISPLAY
-  Jiffy([2021, 1, 19]).format("MMMM do yyyy, h:mm:ss a"); // January 1st 2021, 12:00:00 AM
-  Jiffy().format("EEEE"); // Tuesday
-  Jiffy().format("MMM do yy"); // Mar 2nd 21
-  Jiffy().format("yyyy [escaped] yyyy"); // 2021 escaped 2021
+  Jiffy([2021, 1, 19])
+      .format('MMM do yyyy, h:mm:ss a'); // January 1st 2021, 12:00:00 AM
+  Jiffy().format('EEEE'); // Tuesday
+  Jiffy().format('MMM do yy'); // Mar 2nd 21
+  Jiffy().format('yyyy [escaped] yyyy'); // 2021 escaped 2021
   Jiffy().format(); // 2021-03-02T15:18:29.922343
 
 //  Not passing a string pattern for format method will return an ISO Date format
@@ -16,62 +17,51 @@ Future<int> main() async {
   Jiffy([2019, 10, 19]).yMMMMd; // January 19, 2021
 
   // Using maps
-  Jiffy({
-    'year': 2019,
-    'month': 10,
-    'day': 19,
-    'hour': 19
-  }).yMMMMEEEEdjm; // Monday, October 19, 2020 7:14 PM
+  Jiffy({'year': 2019, 'month': 10, 'day': 19, 'hour': 19})
+      .yMMMMEEEEdjm; // Monday, October 19, 2020 7:14 PM
 
-  // "From Now" implementation
-  Jiffy("2007-1-29").fromNow(); // 14 years ago
+  // 'From Now' implementation
+  Jiffy('2007-1-29').fromNow(); // 14 years ago
   Jiffy([2022, 10, 29]).fromNow(); // in a year
   Jiffy(DateTime(2050, 10, 29)).fromNow(); // in 30 years
 
-  var jiffy1 = (Jiffy()
-    ..startOf(Units.HOUR)
-  ).fromNow(); // 9 minutes ago
+  (Jiffy()..startOf(Units.HOUR)).fromNow(); // 9 minutes ago
 
-//  "From X" implementation
-  var jiffy2 = Jiffy("2007-1-28");
-  var jiffy3 = Jiffy("2017-1-29", "yyyy-MM-dd");
+//  'From X' implementation
+  var jiffy2 = Jiffy('2007-1-28');
+  var jiffy3 = Jiffy('2017-1-29', 'yyyy-MM-dd');
 
   jiffy2.from(jiffy3); // a day ago
 
   jiffy2.from([2017, 1, 30]); // 2 days ago
 
-//  Displaying thid "Difference" between two date times
-//  By default, "diff" method, get the difference in milliseconds
-  var jiffy4 = Jiffy("2007-1-28", "yyyy-MM-dd");
-  var jiffy5 = Jiffy("2017-1-29", "yyyy-MM-dd");
+//  Displaying thid 'Difference' between two date times
+//  By default, 'diff' method, get the difference in milliseconds
+  var jiffy4 = Jiffy('2007-1-28', 'yyyy-MM-dd');
+  var jiffy5 = Jiffy('2017-1-29', 'yyyy-MM-dd');
   jiffy4.diff(jiffy5); // 86400000
 
-  // You can also get "diff" in different units of time
+  // You can also get 'diff' in different units of time
   Jiffy([2007, 1, 28]).diff([2017, 1, 29], Units.DAY); // -3654
 
 //  RELATIVE TIME
   Jiffy('2011-10-31').fromNow(); // 8 years ago
   Jiffy(DateTime(2012, 6, 20)).fromNow(); // 7 years ago
 
-  var jiffy6 = Jiffy()
-    ..startOf(Units.DAY);
+  var jiffy6 = Jiffy()..startOf(Units.DAY);
   jiffy6.fromNow(); // 19 hours ago
 
-  var jiffy7 = Jiffy()
-    ..endOf(Units.DAY);
+  var jiffy7 = Jiffy()..endOf(Units.DAY);
   jiffy7.fromNow(); // in 5 hours
 
-  var jiffy8 = Jiffy()
-    ..startOf(Units.HOUR);
+  var jiffy8 = Jiffy()..startOf(Units.HOUR);
   jiffy8.fromNow(); // 9 minutes ago
 
 //  MANIPULATING DATES
-  var jiffy9 = Jiffy()
-    ..add(duration: Duration(days: 1));
+  var jiffy9 = Jiffy()..add(duration: Duration(days: 1));
   jiffy9.yMMMMd; // October 20, 2019
 
-  var jiffy10 = Jiffy()
-    ..subtract(days: 1);
+  var jiffy10 = Jiffy()..subtract(days: 1);
   jiffy10.yMMMMd; // October 18, 2019
 
 //  You can chain methods by using Dart method cascading
